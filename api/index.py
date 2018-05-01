df951009ca47c10f6069aae2233debd0211164ce
Kenji Yoneyama <kenji.3.yoneyama@gmail.com>
2018-05-01 17:04:14 +0900

    revert api index

--- api/index.py
+++ api/index.py
@@ -1,51 +1,32 @@
-from flask import Flask, render_template, request
+import falcon
+import json
 import subprocess
 
 DUTY_BIN = 25
 DUTY_MAX = 225
 DUTY_MIN = 100
-POS_MIN = -5
-POS_MAX = 5
 
-class Servo:
-    def __init__(self, duty=DUTY_MIN):
-        self.duty = duty
-
-    def set_duty(self, duty):
-        if(duty >= DUTY_MIN and duty <= DUTY_MAX):
-            self.duty = duty
-
-    def set_position(self, position):
-        if(position >= POS_MIN and position <= POS_MAX):
-            duty = DUTY_MIN + (position-POS_MIN)*(DUTY_MAX-DUTY_MIN)/(POS_MAX-POS_MIN)
-            self.duty = duty
-
-    def get_duty(self):
-        return self.duty
-
-    def to_position(self):
-        position = POS_MIN + (self.duty-DUTY_MIN)*(POS_MAX-POS_MIN)/(DUTY_MAX-DUTY_MIN)
-        return position
-
-@app.route("/")
-def index():
-    return render_template("index.html")
-
-@app.route("/servo")
-def servo_route():
-    position = request.args.get('position')
-    duty = int(open('/var/www/api/servo.conf', 'r').read())
-    servo = Servo(duty)
-    if (position):
-        servo.set_position(position)
-        open('/var/www/api/servo.conf', 'w').write('{}'.format(servo.to_duty()))
+class RootResource:
+    def on_get(self, req, resp):
+        action = req.get_param('action')
+        duty = int(open('/var/www/api/servo.conf', 'r').read())
+        if(action == 'swing-left'):
+            duty += DUTY_BIN
+            if(duty > DUTY_MAX):
+                duty = DUTY_MAX
+        if(action == 'swing-right'):
+            duty -= DUTY_BIN
+            if(duty < DUTY_MIN):
+                duty = DUTY_MIN
+        open('/var/www/api/servo.conf', 'w').write('{}'.format(duty))
         subprocess.call('/var/www/api/pwm')
-        ret = {'duty': '{}'.format(servo.get_duty()),
-               'position': '{}'.format(servo.to_position())}
-    else:
-        ret = {'duty': '{}'.format(duty),
-               'position': '{}'.format(position)}
-    return jsonify(ResulfSet=ret)
+        resp.media = {'duty': '{}'.format(duty)}
+
+api = falcon.API()
+api.add_route('/', RootResource())
 
 if __name__ == '__main__':
-    app.run(debug=True)
+  from wsgiref import simple_server
+  httpd = simple_server.make_server("127.0.0.1", 11112, api)
+  httpd.serve_forever()
+
