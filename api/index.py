6d8916d865f016be30de4e55dbacf46bbc50f443
Kenji Yoneyama <kenji.3.yoneyama@gmail.com>
2018-05-01 16:08:36 +0900

    add awsome bootstrap template

--- api/index.py
+++ api/index.py
@@ -5,22 +5,43 @@ import subprocess
 DUTY_BIN = 25
 DUTY_MAX = 225
 DUTY_MIN = 100
+POS_MIN = -5
+POS_MAX = 5
+
+class Servo:
+    def __init__(self, duty=DUTY_MIN):
+        self.duty = duty
+
+    def set_duty(self, duty):
+        if(duty >= DUTY_MIN and duty <= DUTY_MAX):
+            self.duty = duty
+
+    def set_position(self, position):
+        if(position >= POS_MIN and position <= POS_MAX):
+            duty = DUTY_MIN + (position-POS_MIN)*(DUTY_MAX-DUTY_MIN)/(POS_MAX-POS_MIN)
+            self.duty = duty
+
+    def get_duty(self):
+        return self.duty
+
+    def to_position(self):
+        position = POS_MIN + (self.duty-DUTY_MIN)*(POS_MAX-POS_MIN)/(DUTY_MAX-DUTY_MIN)
+        return position
 
 class RootResource:
     def on_get(self, req, resp):
-        action = req.get_param('action')
+        position = req.get_param('position')
         duty = int(open('/var/www/api/servo.conf', 'r').read())
-        if(action == 'swing-left'):
-            duty += DUTY_BIN
-            if(duty > DUTY_MAX):
-                duty = DUTY_MAX
-        if(action == 'swing-right'):
-            duty -= DUTY_BIN
-            if(duty < DUTY_MIN):
-                duty = DUTY_MIN
-        open('/var/www/api/servo.conf', 'w').write('{}'.format(duty))
-        subprocess.call('/var/www/api/pwm')
-        resp.media = {'duty': '{}'.format(duty)}
+        servo = Servo(duty)
+        if (position):
+            servo.set_position(position)
+            open('/var/www/api/servo.conf', 'w').write('{}'.format(servo.to_duty()))
+            subprocess.call('/var/www/api/pwm')
+            resp.media = {'duty': '{}'.format(servo.get_duty()),
+                          'position': '{}'.format(servo.to_position())}
+        else:
+            resp.media = {'duty': '{}'.format(duty),
+                          'position': '{}'.format(position)}
 
 api = falcon.API()
 api.add_route('/', RootResource())
@@ -29,4 +50,3 @@ if __name__ == '__main__':
   from wsgiref import simple_server
   httpd = simple_server.make_server("127.0.0.1", 11112, api)
   httpd.serve_forever()
-
--- api/myapp.py
+++
@@ -1,4 +0,0 @@
-def app(environ, start_response):
-    data = "hello\n"
-    start_response("200 OK", [("Content-Type", "text/plain"), ("Content-Length", str(len(data)))])
-    return iter([data])
